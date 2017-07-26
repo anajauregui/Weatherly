362107b37e2b47b2aa4aef2717e10b5f0c9391d6
Jonathan Beckman <jbecks.media@gmail.com>
2017-07-26 13:21:39 -0600

    add api.js file

--- webpack.config.js
+++ webpack.config.js
@@ -19,13 +19,13 @@ module.exports = {
       },
       { test: /\.css$/, loader: 'style!css' },
       { test: /\.scss$/, loader: 'style!css!sass' },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-            'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
-      },
+      // {
+      //   test: /\.(jpe?g|png|gif|svg)$/i,
+      //   loaders: [
+      //       'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+      //       'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      //   ]
+      // },
     ],
   },
   resolve: {
