42e0fd40a0f0ba0383e7b4634e66801d6454b164
Ana <analujauregui@gmail.com>
2017-07-29 14:55:54 -0600

    Search new Component

--- utils/Api.js
+++ utils/Api.js
@@ -1,5 +1,5 @@
 import Key from './Key';
-import Search from '../lib/Search';
+import Welcome from '../lib/Welcome';
 
 export default function API (location) {
   return `http://api.wunderground.com/api/${Key}/conditions/hourly/forecast10day/q/${location}.json`
