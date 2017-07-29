49ac81cfa740c02c31d9ba90e6d35e2cff3d8ec2
Jonathan Beckman <jbecks.media@gmail.com>
2017-07-28 20:36:27 -0600

    move file into utils folder to better organize files

--- utils/Api.js
+++ utils/Api.js
@@ -1,5 +1,5 @@
 import Key from './Key';
-import Search from './Search';
+import Search from '../lib/Search';
 
 export default function API (city) {
   return `http://api.wunderground.com/api/${Key}/conditions/hourly/forecast10day/q/CO/${city}.json`
