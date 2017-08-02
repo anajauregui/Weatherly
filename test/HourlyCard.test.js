784105a8c89e5e8fb637c75f2f58ac2767df1f44
Jonathan Beckman <jbecks.media@gmail.com>
2017-08-01 18:39:36 -0600

    build out test files and fix some funcitonality

--- test/HourlyCard.test.js
+++ test/HourlyCard.test.js
@@ -10,22 +10,30 @@ describe('HourlyCard', () => {
   beforeEach(() => {
     let hour = weatherData.hourly_forecast[0];
 
-    wrapper = shallow(<HourlyCard
-      temp={hour.temp.english} time={hour.FCTTIME.civil} img={hour.icon_url} />)
+    wrapper = shallow(
+      <HourlyCard
+      temp={hour.temp.english}
+      time={hour.FCTTIME.civil}
+      img={hour.icon_url} />
+    )
   })
 
   it('should exist', () => {
     expect(wrapper).toBeDefined()
   })
 
-  it.skip('should render a img with a className of hourly-weather-pic', () => {
+  it('should render a img with a className of hourly-weather-pic', () => {
     expect(wrapper.find('.hourly-weather-pic')).toBeDefined();
-    expect(wrapper.find('.hourly-weather-pic').text()).toEqual()
+    expect(wrapper.find('.hourly-weather-pic').prop('src')).toBe("http://icons.wxug.com/i/c/k/clear.gif")
   })
 
   it('should render a p with a className of hour-temp', () => {
-    console.log('nnnnnnnnnNNNNNNNN ', wrapper.find('.hour-temp').debug());
     expect(wrapper.find('.hour-temp')).toBeDefined();
     expect(wrapper.find('.hour-temp').text()).toEqual("83 ℉")
+  })
+
+  it('should render a p with a className of hour', () => {
+    expect(wrapper.find('.hour')).toBeDefined();
+    expect(wrapper.find('.hour').text()).toEqual("12:00 PM")
   })
 })
